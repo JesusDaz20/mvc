48f411308f10cdfce943422a4911f38ce5ec2c1f
Jesús Antonio Diaz Cruz <1720110413@utectulancingo.edu.mx>
2022-04-08 13:08:29 +0000

    avance 5

---
+++ mvc/firebase_config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  apiKey: "AIzaSyBrAxrxcCj8O6gptMqELmSIqz4EDGL6o18",
+  authDomain: "temperatura-proyecto-8afdb.firebaseapp.com",
+  databaseURL: "https://temperatura-proyecto-8afdb-default-rtdb.firebaseio.com",
+  projectId: "temperatura-proyecto-8afdb",
+  storageBucket: "temperatura-proyecto-8afdb.appspot.com",
+  messagingSenderId: "470351199573",
+  appId: "1:470351199573:web:c019526d22d6dcb0d4c3dc",
+  measurementId: "G-ZWGPWWHFTH"
+}
