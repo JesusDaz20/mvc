cbf82f1863debca5e85925e886c736c930571e99
Jesús Antonio Diaz Cruz <1720110413@utectulancingo.edu.mx>
2022-04-12 04:43:26 +0000

    avances python

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ urls = (
     '/login', 'mvc.controllers.public.login.Login',
     '/recuperar', 'mvc.controllers.public.recuperar.Recuperar',
     '/bienvenida_admin', 'mvc.controllers.admin.bienvenida_admin.Bienvenida_admin',
-    '/bienvenida_operador', 'mvc.controllers.operador.bienvenida_operador.Bienvenida_operador',
+    '/bienvenida_operador', 'mvc.controllers.operadores.bienvenida_operador.Bienvenida_operador',
     '/usuarios', 'mvc.controllers.admin.usuarios.Usuarios',
     '/agregar', 'mvc.controllers.admin.agregar.Agregar',
     '/sucursales', 'mvc.controllers.admin.sucursales.Sucursales',
--- mvc/controllers/operadores/bienvenida_operador.py
+++ mvc/controllers/operadores/bienvenida_operador.py
@@ -3,16 +3,16 @@ import mvc.firebase_config as token
 import pyrebase
 import app 
 
-render = web.template.render("mvc/views/admin/")
+render = web.template.render("mvc/views/operador/")
 
 class Bienvenida_operador:
     def GET(self): 
-        ("Bienvenida_operador.GET localID: ",web.cookies().get('localId'))
+        ("Bienvenida.GET localID: ",web.cookies().get('localId'))
         try: 
-            ("Bienvenida_operador.GET localID: ",web.cookies().get('localId')) 
-            if web.cookies().get('localId') == "0": 
+            ("Bienvenida.GET localID: ",web.cookies().get('localId')) 
+            if web.cookies().get('localId') == None: 
                 return web.seeother("bienvenida_operador") 
             else: 
                 return render.bienvenida_operador() 
         except Exception as error: 
-            print("Error Bienvenida_operador.GET: {}".format(error))  
+            print("Error Bienvenida.GET: {}".format(error))  
