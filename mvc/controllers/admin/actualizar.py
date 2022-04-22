a7feac653bdb4c7fa088db3d51e94a3c00af1b60
Jesús Antonio Diaz Cruz <1720110413@utectulancingo.edu.mx>
2022-04-22 20:23:02 +0000

    actualizar en proceso 4

--- mvc/controllers/admin/actualizar.py
+++ mvc/controllers/admin/actualizar.py
@@ -5,27 +5,28 @@ import json
 import app
 
 render = web.template.render("mvc/views/admin/")
+firebase = pyrebase.initialize_app(token.firebaseConfig)
+auth = firebase.auth() 
+db = firebase.database()
 
 
 class Actualizar: 
-    def GET(self):
+    def GET(self,localId):
         localId = web.cookies().get('localId')
         all_users = db.child("usuarios").get() 
         user = db.child("usuarios").child(localId).get()
         return render.actualizar(user)
 
     def POST(self):
-        firebase = pyrebase.initialize_app(token.firebaseConfig)
-        auth = firebase.auth() 
-        
         formulario = web.input()
+        id = formulario.id
         name = formulario.name
         phone = formulario.phone
         email = formulario.email
-        password= formulario.password
         level = formulario.level
         status = formulario.status
         datos_usuarios = {'name': name,'phone': phone,'email':email, 'level':level, 'status':status} 
-        db.child("usuarios").child(name).update(datos_usuarios)
+        result = db.child("usuarios").child(name).update(datos_usuarios)
+        print(result)
         return web.seeother('/usuarios') 
        
