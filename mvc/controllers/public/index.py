3de694d922211ef75916120be3bc984c89f5eb2e
Jesús Antonio Diaz Cruz <1720110413@utectulancingo.edu.mx>
2022-04-22 00:46:40 +0000

    login funcionando

--- mvc/controllers/public/index.py
+++ mvc/controllers/public/index.py
@@ -1,9 +1,13 @@
-import app
-import web
- 
+import web 
+import mvc.firebase_config as token
+import pyrebase
+import app 
+import json
+
 render = web.template.render('mvc/views/public/') 
 
 class Index:
     def GET(self):
         return render.index() 
-
+    def POST(self): 
+        return render.index()    
