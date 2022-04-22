d2a20fedce68729a8826d220f241c7314ed15a51
Jesús Antonio Diaz Cruz <1720110413@utectulancingo.edu.mx>
2022-04-22 19:20:59 +0000

    actualizar en proceso 3

--- mvc/controllers/admin/actualizar.py
+++ mvc/controllers/admin/actualizar.py
@@ -8,28 +8,24 @@ render = web.template.render("mvc/views/admin/")
 
 
 class Actualizar: 
-    def GET(self, localId):
+    def GET(self):
         localId = web.cookies().get('localId')
         all_users = db.child("usuarios").get() 
         user = db.child("usuarios").child(localId).get()
         return render.actualizar(user)
 
     def POST(self):
-        try:
-            firebase = pyrebase.initialize_app(token.firebaseConfig)
-            auth = firebase.auth() 
-            db = firebase.database()
-            formulario = web.input()
-            name = formulario.name
-            phone = formulario.phone
-            email = formulario.email
-            password= formulario.password
-            level = formulario.level
-            status = formulario.status
-            datos_usuarios = {'name': name,'phone': phone,'email':email, 'level':level, 'status':status} 
-            result = db.child("usuarios").child(id).update(datos_usuarios)
-            print(result)
-            return web.seeother('/usuarios') 
-        except Exception as error:
-            print("Error actualizar.POST: {}".format(message)) 
-            return web.seeother('/usuarios')  
+        firebase = pyrebase.initialize_app(token.firebaseConfig)
+        auth = firebase.auth() 
+        
+        formulario = web.input()
+        name = formulario.name
+        phone = formulario.phone
+        email = formulario.email
+        password= formulario.password
+        level = formulario.level
+        status = formulario.status
+        datos_usuarios = {'name': name,'phone': phone,'email':email, 'level':level, 'status':status} 
+        db.child("usuarios").child(name).update(datos_usuarios)
+        return web.seeother('/usuarios') 
+       
